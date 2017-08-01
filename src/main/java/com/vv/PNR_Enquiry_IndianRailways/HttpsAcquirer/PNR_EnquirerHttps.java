b6651c05e2597f03a28f182758a856394d2efe9f
crackCodeLogn <vivektheintel@gmail.com>
2017-08-01 20:43:33 +0530

    First commit for the HttpsAcquirer. Has the following changes:- 1) Good amount of Logger with the help of java.util.Logging 2) Clearing up of clutter of code
    Thing to be done next time:-
    1) Get the details of the ticket from the page obtained in the instream

--- src/main/java/com/vv/PNR_Enquiry_IndianRailways/HttpsAcquirer/PNR_EnquirerHttps.java
+++ src/main/java/com/vv/PNR_Enquiry_IndianRailways/HttpsAcquirer/PNR_EnquirerHttps.java
@@ -10,10 +10,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.logging.*;
 
 /**
  * This deals with the Https Acquirer for the pnr request...
+ *
  * @author Vivek
  * @version 1.0
  * @since 01-08-2017
@@ -28,19 +28,21 @@ public class PNR_EnquirerHttps {
         logger = LoggerFormatter.formatTheLoggerOutput(logger);
         //the above line should be written in each and every class where logging is to be used
 
-        HttpsURLConnection httpsURLConnection = null;
-        URL url = new URL("https://www.railyatri.in/pnr-status/"+pnr1); //giving 404 resp
-        //logger.info("URL : "+url.toString());
-        logger.info("URL : "+url.toString());
-        System.out.println("URL : "+url.toString());
+        int responseCode = 404;
 
+        HttpsURLConnection httpsURLConnection = null;
+        URL url = new URL("https://www.railyatri.in/pnr-status/" + pnr1); //this https enabled site was allowing automated data extraction without giving any forbidden access response code
+        logger.info("URL : " + url.toString());
+
+        logger.info("Establishing connection now!");
         try {
             httpsURLConnection = (HttpsURLConnection) url.openConnection();
-            httpsURLConnection.setDoOutput(true);
-            httpsURLConnection.setRequestMethod("POST");
+            //httpsURLConnection.setDoOutput(true);
+            //httpsURLConnection.setRequestMethod("POST");
             httpsURLConnection.connect();
 
             /*
+            This entire block of statements were commented out as we are passing no parameter (i.e. the PNE) to this url based page
             PrintWriter pw = new PrintWriter(httpsURLConnection.getOutputStream());
             pw.write("lccp_pnrno1="+pnr1);
             //pw.write("fullname="+pnr1);
@@ -51,25 +53,31 @@ public class PNR_EnquirerHttps {
             pw.flush();
             pw.close();
             */
-            logger.info("Code received : " + httpsURLConnection.getResponseCode());
+            responseCode = httpsURLConnection.getResponseCode();
+            logger.info("Code received : " + responseCode);
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                logger.info("Connection established successfully!!");
+            }
         } catch (Exception e) {
-            logger.info("Exception : " + e);
+            logger.info("Connection establishment failed... \nException : " + e);
         }
 
+        logger.info("Establishing inflow path now, if possible...");
         try {
-            //BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-            //BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-            int lineNo = 1;
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null) {
-                logger.info("Line number " + (lineNo++) + " --> " + line);
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
+                int lineNo = 1;
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    logger.info("Line number " + (lineNo++) + " --> " + line);
+                }
+                bufferedReader.close();
             }
-            //logger.info("Lines:-\n"+line);
-            logger.info("Overshooting the line number : " + lineNo);
-            bufferedReader.close();
+            else{
+                logger.info("No reading of webpage as connection not ok!");
+            }
         } catch (Exception e) {
-            logger.info("Exception in the reading part... \n\tException : "+e);
+            logger.info("Exception in the reading part... \n\tException : " + e);
         }
     }
 }
